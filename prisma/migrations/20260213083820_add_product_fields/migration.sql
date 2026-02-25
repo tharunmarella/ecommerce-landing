1fcaa2cc2bd6af4ea65686847b55394c8028751b
Tharun Marella <tharunmarella@gmail.com>
2026-02-24 21:59:40 -0500

    refactor: remove old migration files

--- prisma/migrations/20260213083820_add_product_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "brand" TEXT,
-ADD COLUMN     "category" TEXT,
-ADD COLUMN     "description" TEXT,
-ADD COLUMN     "inStock" BOOLEAN NOT NULL DEFAULT true;
